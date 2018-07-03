5aa8dde22c00ba0050592a77b00b54b85c90622b
vesna <vesnaradovic83@gmail.com>
2018-07-03 11:55:06 +0200

    add GET /todos

--- server/server.js
+++ server/server.js
@@ -48,6 +48,14 @@ app.listen(3000, () => {
   console.log('Started on port 3000')
 });
 
+app.get('/todos', (req,res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 
 
 
