812e5ddb674e84d4db51f17179524356b7cc75ca
vesna <vesnaradovic83@gmail.com>
2018-07-04 15:23:36 +0200

    add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
+const _ = require('lodash');
 const express = require('express');
-
 const bodyParser = require('body-parser');
-
 const {ObjectID} = require('mongodb');
 
 
@@ -55,6 +54,31 @@ app.get('/todos/:id', (req,res) => {
     res.status(400).send();
   })
       
+});
+
+app.patch('/todos/:id', (req,res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null; 
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
 });
 
 app.listen(port, () => {
