3988dcfcbc4226e3811415772b7d82b6d26336a9
vesna <vesnaradovic83@gmail.com>
2018-07-05 14:32:44 +0200

    set up separate test aand development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,12 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ToDoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 
 
 //when someone requires this file, they're going to have mongoose configured and they're gonna get it back; 
 //they get the mongoose variable that comes from a library
 module.exports = {mongoose};
+
+
