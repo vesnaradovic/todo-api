5bfe4a0d4aae7bf6e208050896fdabea8207962a
vesna <vesnaradovic83@gmail.com>
2018-07-12 16:54:39 +0200

    add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -46,7 +46,7 @@ UserSchema.methods.generateAuthToken = function () {
   var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
  
 
-user.tokens.concat(access, token);
+user.tokens.concat([access, token]);
 
 
 return user.save().then(() => {
@@ -70,6 +70,26 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.token': token,
     'tokens.access': 'auth'
   });
+};
+
+UserSchema.statics.findByCredentials = function  (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      })
+    })
+  })
 };
 
 
