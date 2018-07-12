470e8cde21daa0a26e244b65a5ad16d1c651f67d
vesna <vesnaradovic83@gmail.com>
2018-07-12 17:56:46 +0200

    add tests for DELETE /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -4,6 +4,34 @@ const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 const bcrypt = require('bcryptjs');
 
+var UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true, 
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  }, 
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true,
+    }, 
+    token: {
+      type: String,
+      required: true
+    }
+  }]
+});
 
 UserSchema.methods.toJSON = function () {
   var user = this;
@@ -25,30 +53,6 @@ return user.save().then(() => {
   return token;
   });
 };
-var UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    trim: true, 
-    minlength: 1,
-    unique: true,
-    validate: {
-      validator: validator.isEmail,
-      message: '{VALUE} is not a valid email'
-    }
-  }, 
-  password: {
-    type: String,
-    require: true,
-    minlength: 6
-  },
-  tokens: [{
-    access: 'auth',
-    token: jwt.sign({_id: user._id, access: 'auth'}, 'abc123').toString()
-  }]
-});
-
-
 
 
 UserSchema.methods.removeToken = function (token) {
